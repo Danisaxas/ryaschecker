13163d0559a505ad4723e2d5c3b0a93e6a3e0079
Danisaxas <danluismay@gmail.com>
2025-03-08 10:28:41 -0400

    update

--- ryas_plugins/Inicio/start.py
+++ ryas_plugins/Inicio/start.py
@@ -1,16 +1,26 @@
 from configs.def_main import *
 @ryas("start")
-async def start(client, msg):
-    username = msg.from_user.username or "Usuario"
-    
-    # Obtener la fecha y hora actual en Caracas, Venezuela con el formato correcto
-    caracas_time = datetime.now(pytz.timezone("America/Caracas")).strftime("%Y-%m-%d Venezuela, Caracas %I:%M %p")
+async def start(client, message):
+    user_id = message.from_user.id
+    conn, cursor = connect_db()
 
-    # Generar el mensaje
+    cursor.execute("""
+        SELECT rango, creditos, antispam, expiracion
+        FROM users
+        WHERE user_id = %s
+    """, (user_id,))
+    user_data = cursor.fetchone()
+
+    if not user_data:
+        await message.reply(register_not, reply_to_message_id=message.id)
+        return
+
+    username = message.from_user.username or "Usuario"
+    caracas_time = datetime.now(pytz.timezone("America/Caracas")).strftime("%Y-%m-%d Venezuela, Caracas %I:%M %p")
     response = startx.format(username=username, caracas_time=caracas_time)
 
-    await msg.reply_text(
+    await message.reply_text(
         response,
-        reply_to_message_id=msg.id,
+        reply_to_message_id=message.id,
         reply_markup=mainstart
     )
--- ryas_plugins/markup/tools.py
+++ ryas_plugins/markup/tools.py
@@ -1,7 +1,6 @@
 from configs.def_main import *
 @ryasbt("^tools$")
 async def tools_callback(client, callback_query: CallbackQuery):
-    # Envía el menú de herramientas
     await callback_query.message.edit_text(
         tools,
         reply_markup=atras
@@ -9,11 +8,9 @@ async def tools_callback(client, callback_query: CallbackQuery):
 
 @ryasbt("^home$")
 async def home_callback(client, callback_query: CallbackQuery):
-    # Obtiene el nombre de usuario y la hora de Caracas
     username = callback_query.from_user.username or "Usuario"
     caracas_time = datetime.now(pytz.timezone("America/Caracas")).strftime("%Y-%m-%d Venezuela, Caracas %I:%M %p")
 
-    # Regresa al mensaje de inicio
     await callback_query.message.edit_text(
         startx.format(username=username, caracas_time=caracas_time),
         reply_markup=mainstart
