71c335a47194347e85042c3137b72e07a9722c38
Danisaxas <danluismay@gmail.com>
2025-03-09 08:55:40 -0400

    add setpriv.py

--- ryas_templates/chattext.py
+++ ryas_templates/chattext.py
@@ -9,13 +9,13 @@ Chat_ID: <code>{chat_id}</code>
 </b>'''
 
 metext = '''<b>
-Ryas Chk ⺄1�7 => User Info
+Ryas Chk ⺄1�7 => User Info
 - - - - - - - - - - - - - - - - - - - - - - - - - - 
-⺄1�7 User:
+⺄1�7 User:
 Username: @{username} [<code>{user_id}</code>]
 First Name: {firts_name} | Ban: No
 - - - - - - - - - - - - - - - - - - - - - - - - - - 
-⺄1�7 Membership:
+⺄1�7 Membership:
 Role: {rango} | Credits: {creditos}
 Antispam: {antispam}
 Expiracion: 0
@@ -25,12 +25,12 @@ register_not = '''<b> No estas registrado en la base de datos usa /register</b>'
 
 startx = '''<b>
 ¡Hola! {username} 🍄, Bienvenido a RyasChk. ¡Relájate y recarga energías!
-━━━━━━━━━━━━┄1�7
+━━━━━━━━━━━━┄1�7
 Hoy es: 
 {caracas_time} 💤
-━━━━━━━━━━━━┄1�7
+━━━━━━━━━━━━┄1�7
 [🇪🇸] @{username}, ¡listo para usar ryas? Soy tu bot personal para checkear tarjetas. Usa /cmds para ver qué puedo hacer.
-━━━━━━━━━━━━┄1�7
+━━━━━━━━━━━━┄1�7
 </b>'''
 
 tools = '''<b>
@@ -38,23 +38,23 @@ RyasChk Tools / Page 1
 ━━━━━━━━━━━━
 Bin Generator:
 Format: $gen 601120  
-Condition: Online! ✄1�7
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 Sk Ckeck:
 Format: $sk sk_live 
-Condition: Online! ✄1�7
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 BIN Lookup:
 Format: $bin 601120 
-Condition: Online! ✄1�7
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 Gen Address:
 Format: $dir Country_code 
-Condition: Online! ✄1�7
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 IP Fraud Check:
 Format: $ip 1.1.1.1 
-Condition: Online! ✄1�7
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 </b>'''
 
@@ -65,29 +65,30 @@ Channels:
 Onyx Updates: @coming soon
 Onyx References channel: @coming soon
 Onyx Free Users: @coming soon
-━━━━━━━━━━━━━━━━┄1�7
+━━━━━━━━━━━━━━━━┄1�7
 Ryas Information:
-━━━━┄1�7
-Dev: @Exzzex ✄1�7 | [Buy]
+━━━━┄1�7
+Dev: @Exzzex ✄1�7 | [Buy]
 Dev Note: Hi guys, this is the new version of onyx, made with speed and good checking experience in mind!
-━━━━┄1�7
+━━━━┄1�7
 Sellers:
-@coming soon | coming soon ✄1�7 | [Buy]
-━━━━━━━━━━━━━━━━┄1�7
+@coming soon | coming soon ✄1�7 | [Buy]
+━━━━━━━━━━━━━━━━┄1�7
 Onyx Updates:
 Version: 1.3.0 [✅]
 Update: coming soon p.m (GMT-5) [✅]
-━━━━━━━━━━━━━━━━┄1�7
+━━━━━━━━━━━━━━━━┄1�7
 Report problems to: @Exzzex
 </b>'''
 
 close_text = '''<b>
 Good bye! 🌩
-━━━━━━━━━━━━━━━━┄1�7
+━━━━━━━━━━━━━━━━┄1�7
 Enjoy my use.
 </b>'''
 
-not_privilegios = '''<b> No cuentas con los privilegios suficientes para usar este comando </b>'''
+not_privilegios = '''<b> No cuentas con los privilegios suficientes para usar este comando.</b>'''
+
 
 
 
