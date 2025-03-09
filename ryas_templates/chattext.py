78b1755b42b6561c34ba91a2850b78484c6de12d
Danisaxas <danluismay@gmail.com>
2025-03-09 08:42:22 -0400

    add setpriv.py

--- ryas_templates/chattext.py
+++ ryas_templates/chattext.py
@@ -9,13 +9,13 @@ Chat_ID: <code>{chat_id}</code>
 </b>'''
 
 metext = '''<b>
-Ryas Chk ⺢ => User Info
+Ryas Chk ⺄1�7 => User Info
 - - - - - - - - - - - - - - - - - - - - - - - - - - 
-⺢ User:
+⺄1�7 User:
 Username: @{username} [<code>{user_id}</code>]
 First Name: {firts_name} | Ban: No
 - - - - - - - - - - - - - - - - - - - - - - - - - - 
-⺢ Membership:
+⺄1�7 Membership:
 Role: {rango} | Credits: {creditos}
 Antispam: {antispam}
 Expiracion: 0
@@ -25,12 +25,12 @@ register_not = '''<b> No estas registrado en la base de datos usa /register</b>'
 
 startx = '''<b>
 ¡Hola! {username} 🍄, Bienvenido a RyasChk. ¡Relájate y recarga energías!
-━━━━━━━━━━━━━
+━━━━━━━━━━━━┄1�7
 Hoy es: 
 {caracas_time} 💤
-━━━━━━━━━━━━━
+━━━━━━━━━━━━┄1�7
 [🇪🇸] @{username}, ¡listo para usar ryas? Soy tu bot personal para checkear tarjetas. Usa /cmds para ver qué puedo hacer.
-━━━━━━━━━━━━━
+━━━━━━━━━━━━┄1�7
 </b>'''
 
 tools = '''<b>
@@ -38,23 +38,23 @@ RyasChk Tools / Page 1
 ━━━━━━━━━━━━
 Bin Generator:
 Format: $gen 601120  
-Condition: Online! ✅
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 Sk Ckeck:
 Format: $sk sk_live 
-Condition: Online! ✅
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 BIN Lookup:
 Format: $bin 601120 
-Condition: Online! ✅
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 Gen Address:
 Format: $dir Country_code 
-Condition: Online! ✅
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 IP Fraud Check:
 Format: $ip 1.1.1.1 
-Condition: Online! ✅
+Condition: Online! ✄1�7
 ━━━━━━━━━━━━
 </b>'''
 
@@ -65,24 +65,28 @@ Channels:
 Onyx Updates: @coming soon
 Onyx References channel: @coming soon
 Onyx Free Users: @coming soon
-━━━━━━━━━━━━━━━━━
+━━━━━━━━━━━━━━━━┄1�7
 Ryas Information:
-━━━━━
-Dev: @Exzzex ✅ | [Buy]
+━━━━┄1�7
+Dev: @Exzzex ✄1�7 | [Buy]
 Dev Note: Hi guys, this is the new version of onyx, made with speed and good checking experience in mind!
-━━━━━
+━━━━┄1�7
 Sellers:
-@coming soon | coming soon ✅ | [Buy]
-━━━━━━━━━━━━━━━━━
+@coming soon | coming soon ✄1�7 | [Buy]
+━━━━━━━━━━━━━━━━┄1�7
 Onyx Updates:
 Version: 1.3.0 [✅]
 Update: coming soon p.m (GMT-5) [✅]
-━━━━━━━━━━━━━━━━━
+━━━━━━━━━━━━━━━━┄1�7
 Report problems to: @Exzzex
 </b>'''
 
 close_text = '''<b>
 Good bye! 🌩
-━━━━━━━━━━━━━━━━━
+━━━━━━━━━━━━━━━━┄1�7
 Enjoy my use.
 </b>'''
+
+Not_permission = '''<b> no tienes permiso para usar este comando</b>'''
+
+
