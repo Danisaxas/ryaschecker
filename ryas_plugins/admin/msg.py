7b6863510acd8cd4da7b170d8ea3f472d7dbc3b3
Danisaxas <danluismay@gmail.com>
2025-03-10 15:06:58 -0400

    add /admin/msg.py

--- ryas_plugins/admin/msg.py
+++ ryas_plugins/admin/msg.py
@@ -12,25 +12,28 @@ async def send_message(client, message):
         await message.reply(not_privilegios, reply_to_message_id=message.id)
         return
 
-    args = message.text.split(" ", 2)
-    if len(args) < 2:
+    if not message.reply_to_message and len(message.text.split()) < 2:
+        await message.reply("Admin panel = /msg id or (message)", reply_to_message_id=message.id)
         return
 
-    if args[1].startswith("-"):
+    if message.reply_to_message:
+        msg_text = message.reply_to_message.text
+    else:
+        args = message.text.split(" ", 2)
+        if len(args) < 2:
+            return
+        msg_text = args[1] if len(args) == 2 else args[2]
+
+    if args[1].startswith("-") or args[1].isdigit():
         target_id = int(args[1])
         cursor.execute("SELECT user_id FROM users WHERE user_id = %s", (target_id,))
         if cursor.fetchone():
-            await client.send_message(target_id, args[2])
-    elif args[1].isdigit():
-        target_id = int(args[1])
-        cursor.execute("SELECT user_id FROM users WHERE user_id = %s", (target_id,))
-        if cursor.fetchone():
-            await client.send_message(target_id, args[2])
+            await client.send_message(target_id, msg_text)
     else:
         cursor.execute("SELECT user_id FROM users")
         users = cursor.fetchall()
         for user in users:
-            await client.send_message(user[0], " ".join(args[1:]))
-    
+            await client.send_message(user[0], msg_text)
+
     cursor.close()
     conn.close()
