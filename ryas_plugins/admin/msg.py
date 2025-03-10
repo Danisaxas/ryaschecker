8b186a642b0a3d4c139db4f611a16a00fc0b829d
Danisaxas <danluismay@gmail.com>
2025-03-10 15:11:40 -0400

    add /admin/msg.py

--- ryas_plugins/admin/msg.py
+++ ryas_plugins/admin/msg.py
@@ -1,4 +1,5 @@
 from configs.def_main import *
+from ryas_templates.chattext import not_privilegios
 
 @ryas("msg")
 async def send_message(client, message):
@@ -13,24 +14,33 @@ async def send_message(client, message):
         return
 
     if message.reply_to_message:
-        msg_text = message.reply_to_message.text
+        forwa_message = message.reply_to_message
     else:
         args = message.text.split(" ", 2)
         if len(args) < 2:
             await message.reply("Admin panel = /msg id or (message)", reply_to_message_id=message.id)
             return
+
         msg_text = args[1] if len(args) == 2 else args[2]
 
-    if not message.reply_to_message and (args[1].startswith("-") or args[1].isdigit()):
-        target_id = int(args[1])
-        cursor.execute("SELECT user_id FROM users WHERE user_id = %s", (target_id,))
-        if cursor.fetchone():
-            await client.send_message(target_id, msg_text)
-    else:
-        cursor.execute("SELECT user_id FROM users")
-        users = cursor.fetchall()
-        for user in users:
-            await client.send_message(user[0], msg_text)
+        if args[1].startswith("-") or args[1].isdigit():
+            target_id = int(args[1])
+            cursor.execute("SELECT user_id FROM users WHERE user_id = %s", (target_id,))
+            if cursor.fetchone():
+                await client.forward_messages(target_id, message.chat.id, message.id)
+        else:
+            cursor.execute("SELECT user_id FROM users")
+            users = cursor.fetchall()
+            for user in users:
+                await client.send_message(user[0], msg_text)
+            cursor.close()
+            conn.close()
+            return
+
+    cursor.execute("SELECT user_id FROM users")
+    users = cursor.fetchall()
+    for user in users:
+        await client.forward_messages(user[0], message.chat.id, forwa_message.id)
 
     cursor.close()
     conn.close()
