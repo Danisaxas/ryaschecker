81e436178c724f238c5a25d540723504fb2c706f
Danisaxas <danluismay@gmail.com>
2025-03-10 15:15:29 -0400

    add /admin/msg.py

--- ryas_plugins/admin/msg.py
+++ ryas_plugins/admin/msg.py
@@ -1,5 +1,4 @@
 from configs.def_main import *
-from ryas_templates.chattext import not_privilegios
 
 @ryas("msg")
 async def send_message(client, message):
@@ -14,33 +13,37 @@ async def send_message(client, message):
         return
 
     if message.reply_to_message:
-        forwa_message = message.reply_to_message
+        msg_to_forward = message.reply_to_message
     else:
         args = message.text.split(" ", 2)
         if len(args) < 2:
             await message.reply("Admin panel = /msg id or (message)", reply_to_message_id=message.id)
             return
 
+        msg_to_forward = None
         msg_text = args[1] if len(args) == 2 else args[2]
 
         if args[1].startswith("-") or args[1].isdigit():
             target_id = int(args[1])
             cursor.execute("SELECT user_id FROM users WHERE user_id = %s", (target_id,))
             if cursor.fetchone():
-                await client.forward_messages(target_id, message.chat.id, message.id)
-        else:
-            cursor.execute("SELECT user_id FROM users")
-            users = cursor.fetchall()
-            for user in users:
-                await client.send_message(user[0], msg_text)
+                await client.send_message(target_id, msg_text)
             cursor.close()
             conn.close()
             return
+        else:
+            msg_to_forward = None
 
-    cursor.execute("SELECT user_id FROM users")
-    users = cursor.fetchall()
-    for user in users:
-        await client.forward_messages(user[0], message.chat.id, forwa_message.id)
+    if msg_to_forward:
+        cursor.execute("SELECT user_id FROM users")
+        users = cursor.fetchall()
+        for user in users:
+            await client.forward_messages(user[0], message.chat.id, msg_to_forward.id)
+    else:
+        cursor.execute("SELECT user_id FROM users")
+        users = cursor.fetchall()
+        for user in users:
+            await client.send_message(user[0], msg_text)
 
     cursor.close()
     conn.close()
