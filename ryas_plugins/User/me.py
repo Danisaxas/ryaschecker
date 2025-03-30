a72857b207bc80b378cba776925934c01194c237
Danisaxas <danluismay@gmail.com>
2025-03-30 18:54:12 -0400

    update

--- ryas_plugins/User/me.py
+++ ryas_plugins/User/me.py
@@ -1,27 +1,42 @@
-from configs.def_main import *
+from configs.def_main import * # Importando configuraciones
+
+# Decorador para el comando "me"
 @ryas('me')
 async def me_command(client, message):
-    user_id = message.from_user.id
-    conn, cursor = connect_db()
+    """
+    Muestra la información del usuario que usa el comando.
 
+    Parámetros:
+        client: El cliente del bot (ej., Telegram Bot API).
+        message: El mensaje que activó el comando.
+    """
+    user_id = message.from_user.id  # Obteniendo el ID del usuario
+    connection, cursor = connect_db()  # Conectando a la base de datos
+
+    # Preparando el query SQL para obtener la información del usuario
     cursor.execute("""
         SELECT rango, creditos, antispam, expiracion
         FROM users
         WHERE user_id = %s
-    """, (user_id,))
-    user_data = cursor.fetchone()
+    """, (user_id,))  # Usando una tupla para el parámetro
+
+    user_data = cursor.fetchone()  # Obteniendo los resultados del query
 
     if user_data:
-        rango, creditos, antispam, expiracion = user_data
-        formatted_metext = metext.format(
+        # Si encontramos al usuario en la base de datos...
+        rango, creditos, antispam, expiration = user_data  # Desempaquetando los datos
+
+        # Formateando el mensaje de respuesta con la información del usuario
+        formatted_text = metext.format(  # Asumiendo que 'metext' es un string de formato
             username=message.from_user.username,
             user_id=user_id,
-            firts_name=message.from_user.first_name,
+            firts_name=message.from_user.first_name,  # ¡Ojo! 'firts_name' está mal escrito, debería ser 'first_name'
             rango=rango,
             creditos=creditos,
             antispam=antispam,
-            expiracion=expiracion or 'No expiration'
+            expiration=expiration or 'No expiration'  # Si expiration es None, pone "No expiration"
         )
-        await message.reply(formatted_metext, reply_to_message_id=message.id)
+        await message.reply(formatted_text, reply_to_message_id=message.id)
     else:
-        await message.reply(register_not, reply_to_message_id=message.id)
+        # Si no encontramos al usuario en la base de datos...
+        await message.reply(register_not, reply_to_message_id=message.id)  # Asumiendo que 'register_not' es el mensaje de error
