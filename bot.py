ed4c0dc92a9dfc0c600fcaa582eaed5fd21b5820
Danisaxas <danluismay@gmail.com>
2025-04-04 22:15:34 -0400

    update 1.0.3

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ from pyrogram import Client
 from configs.def_main import *
 import asyncio
 from datetime import datetime
-import pytz
+import pytz  # Importa pytz
+from pyrogram.errors import KeyError  # Importa KeyError
 
 logging.basicConfig(level=logging.INFO)
 
@@ -33,12 +34,25 @@ async def main():
         caracas_timezone = pytz.timezone("America/Caracas")
         now = datetime.now(caracas_timezone).strftime("%Y-%m-%d %H:%M:%S %Z")
         
-        # Enviar el mensaje al propietario del bot
-        await Ryas.send_message(
-            chat_id=OWNER_ID,
-            text=f"RyasChk ha encendido.\nEl estado del bot es ONN ✅\nHora de encendido: {now}"
-        )
-        logging.info("Bot started and notification sent to owner.")
+        try:
+            # Tentar enviar el mensaje directamente
+            await Ryas.send_message(
+                chat_id=OWNER_ID,
+                text=f"RyasChk ha encendido.\nEl estado del bot es ONN ✅\nHora de encendido: {now}"
+            )
+            logging.info("Bot started and notification sent to owner.")
+        except KeyError:
+            # Si da KeyError, obtener la información del usuario y reintentar
+            try:
+                owner_user = await Ryas.get_users(OWNER_ID)
+                await Ryas.send_message(
+                    chat_id=owner_user.id,  # Use the user ID from get_users
+                    text=f"RyasChk ha encendido.\nEl estado del bot es ONN ✅\nHora de encendido: {now}"
+                )
+                logging.info("Bot started, user info fetched, and notification sent to owner.")
+            except Exception as e:
+                # Manejar cualquier otro error al obtener el usuario o enviar el mensaje
+                logging.error(f"Error sending startup message: {e}")
 
     
 Ryas.run(main())
