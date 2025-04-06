efb39edfe27f0a27b1171ef21e0a8c6908a746a9
Danisaxas <danluismay@gmail.com>
2025-04-05 20:30:21 -0400

    update 1.1.0

--- ryas_plugins/markup/es.py
+++ ryas_plugins/markup/es.py
@@ -1,4 +1,3 @@
-# markup/es.py
 from configs.def_main import *
 from pyrogram import Client, types
 
@@ -12,20 +11,26 @@ async def handle_es_button(client: Client, callback_query: types.CallbackQuery):
         # Conecta a la base de datos
         connection, cursor = connect_db()
 
+        # Verifica el idioma actual del usuario
+        cursor.execute("SELECT lang FROM users WHERE user_id = %s", (callback_query.from_user.id,))
+        current_lang = cursor.fetchone()[0] if cursor.fetchone() else 'es'
+
+        if current_lang == 'es':
+            await callback_query.message.edit_text(
+                "Ya tienes puesto el idiooma Español 🇪🇸",
+                reply_markup=back
+            )
+        else:
         # Actualiza el idioma del usuario en la tabla 'users'
-        user_id = callback_query.from_user.id
-        update_query = "UPDATE users SET lang = 'es' WHERE user_id = %s"
-        cursor.execute(update_query, (user_id,))
-        connection.commit()
+            update_query = "UPDATE users SET lang = 'es' WHERE user_id = %s"
+            cursor.execute(update_query, (callback_query.from_user.id,))
+            connection.commit()
 
-        # Cargar los botones en el idioma correspondiente
-        from ryas_templates.botones import es as botones_dict
-
-        # Responde al usuario con un mensaje de confirmación y el teclado 'back'
-        await callback_query.message.edit_text(
-            "Se ha puesto el idioma Español 🇪🇸",
-            reply_markup=botones_dict['back']  # Usa el teclado del idioma correspondiente
-        )
+            # Responde al usuario con un mensaje de confirmación y el teclado 'back'
+            await callback_query.message.edit_text(
+                "Se ha puesto el idioma Español 🇪🇸",
+                reply_markup=back
+            )
 
     except Exception as e:
         print(f"Error en handle_es_button: {e}")
