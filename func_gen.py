b32cb5a855c3eae5780bf189da016a2dbcdaff04
Danisaxas <danluismay@gmail.com>
2025-04-08 09:53:49 -0400

    sx

--- func_gen.py
+++ func_gen.py
@@ -1,4 +1,5 @@
 import random
+from configs.def_main import *
 
 def luhn_verification(num):
     num = [int(d) for d in str(num)]
@@ -14,9 +15,7 @@ def luhn_verification(num):
     total = total * 9
     return (total % 10) == check_digit
 
-
-
-def cc_gen(cc,mes='x',ano='x',cvv='x'):
+def cc_gen(cc, mes='x', ano='x', cvv='x'):
     ccs = []
     while len(ccs) < 10:
         card = str(cc)
@@ -29,50 +28,64 @@ def cc_gen(cc,mes='x',ano='x',cvv='x'):
         list_emty = []
 
         for i in new_list:
-            if i =='x':
-                list_emty.append(str(random.randint(0,9)))
+            if i == 'x':
+                list_emty.append(str(random.randint(0, 9)))
             else:
                 list_emty.append(i)
 
         list_empty_string = ''.join(list_emty)
         card = list_empty_string
 
-
-
-
+        # Ajustar la longitud de la tarjeta dependiendo del primer dígito
         if card[0] == '3':
             card = card[0:15]
         else:
             card = card[0:16]
 
-        if mes == 'x':
-            mes_gen = random.randint(1,12)
-            if len(str(mes_gen)) == 1:
-                mes_gen = '0' + str(mes_gen)
+        # Generar mes: si se pasó "rnd", se genera un valor aleatorio por iteración
+        if mes.lower() == "rnd":
+            mes_gen = f"{random.randint(1, 12):02d}"
+        elif mes == 'x':
+            mes_gen = random.randint(1, 12)
+            mes_gen = f"{mes_gen:02d}"
         else:
-            mes_gen = mes
+            mes_gen = mes[0:2]
 
-        if ano == 'x':
-            ano_gen = random.randint(2023,2031)
+        # Generar año: si se pasó "rnd", se genera un valor aleatorio por iteración
+        if ano.lower() == "rnd":
+            # Rango de años: desde el año actual hasta 5 años adelante (puedes ajustar el rango)
+            current_year = datetime.now().year
+            ano_gen = str(random.randint(current_year, current_year + 5))
+        elif ano == 'x':
+            ano_gen = random.randint(2023, 2031)
+            ano_gen = str(ano_gen)
         else:
             ano_gen = ano
             if len(str(ano_gen)) == 2:
                 ano_gen = '20' + str(ano_gen)
 
-        if cvv == 'x':
-            if card[0] == '3':
-                cvv_gen = random.randint(1000,9999)
+        # Generar cvv: si se pasó "rnd", se genera un cvv aleatorio distinto por cada tarjeta
+        if cvv.lower() == "rnd":
+            if card[0] == '3':  # Puede corresponder a American Express
+                cvv_gen = random.randint(1000, 9999)
             else:
-                cvv_gen = random.randint(100,999)
+                cvv_gen = random.randint(100, 999)
+            cvv_gen = str(cvv_gen)
+        elif cvv == 'x':
+            if card[0] == '3':
+                cvv_gen = random.randint(1000, 9999)
+            else:
+                cvv_gen = random.randint(100, 999)
+            cvv_gen = str(cvv_gen)
         else:
             cvv_gen = cvv
 
-        x = str(card) + '|' + str(mes_gen) + '|' + str(ano_gen) + '|' + str(cvv_gen) + '\n'  
-        a = luhn_verification(card)
-        if a:
+        # Genera la tarjeta en el formato deseado
+        x = str(card) + '|' + str(mes_gen) + '|' + str(ano_gen) + '|' + str(cvv_gen) + '\n'
+        # Verificar con el algoritmo Luhn
+        if luhn_verification(card):
             ccs.append(x)
         else:
             continue
-
 
     return ccs
