32647bb07ef345f8692759cb4347e52b7b77631e
Danisaxas <danluismay@gmail.com>
2025-04-13 23:06:53 -0400

    full

--- complemet/User/me.py
+++ complemet/User/me.py
@@ -12,7 +12,7 @@ async def me_command(client: Client, message: types.Message):
         full_name = f"{message.from_user.first_name or ''} {message.from_user.last_name or ''}".strip()
         user_lang = message.from_user.language_code or 'es'
         user_lang = 'en' if user_lang.startswith('en') else 'es'
-        db = MondB(id=user_id, username=username, name=full_name, idchat=user_id)
+        db = MondB(_id=user_id, username=username, name=full_name, idchat=user_id)
         user_data = db.queryUser()
         if not user_data:
             if user_lang == 'en':
@@ -20,12 +20,12 @@ async def me_command(client: Client, message: types.Message):
             else:
                 await message.reply_text(text_es['register_not'], reply_to_message_id=message.id)
             return
-        rango = user_data.get("plan", "Free User")
-        creditos = user_data.get("credits", 0)
-        antispam = user_data.get("antispam", 60)
-        expiration = user_data.get("expiracion", "No plan contrated")
-        lang = user_data.get("lang", "es")
-        ban = user_data.get("status", "Libre")
+        rango = user_data["plan"]
+        creditos = user_data["credits"]
+        antispam = user_data["antispam"]
+        expiration = user_data["expiracion"]
+        lang = user_data["lang"]
+        ban = user_data["status"]
         if lang == 'en':
             text_dict = text_en
         else:
