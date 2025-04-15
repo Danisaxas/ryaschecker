2c087dfd234747a092002d07b546c0b6c21b63c0
Danisaxas <danluismay@gmail.com>
2025-04-14 20:12:49 -0400

    full

--- complemet/markup/es.py
+++ complemet/markup/es.py
@@ -8,9 +8,9 @@ async def handle_es_button(client: Client, callback_query: types.CallbackQuery):
     try:
         user_id = callback_query.from_user.id
         db = MondB(idchat=user_id)
-        user = db.queryUser()
-        if user:
-            db.updateUser({"lang": "es"})
+        _database = db._client['bot']
+        _collection = _database['user']
+        _collection.update_one({"id": user_id}, {"$set": {"lang": "es"}})
         await callback_query.message.edit_text(
             """Cloud DB | LANG [🇪🇸] 
 
@@ -18,5 +18,6 @@ Exito! Ahora su idioma seleccionado es [Español]!""",
             reply_markup=botones_es['back_lang']
         )
     except Exception as e:
-        print(f"Error en handle_es_button: {e}")
         await callback_query.message.edit_text(f"Ocurrió un error: {e}", reply_markup=None)
+    finally:
+        db._client.close()
