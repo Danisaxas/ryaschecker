8f7edb12a43d8842df5536825fc426c9f990a94b
Danisaxas <danluismay@gmail.com>
2025-05-19 19:38:00 -0400

    Actualizar Key.py

--- complemet/admin/Key.py
+++ complemet/admin/Key.py
@@ -13,18 +13,6 @@ async def key_handler(client, message):
     user_lang = 'en' if user_lang.startswith('en') else 'es'
 
     args = message.text.split()
-    if len(args) < 2 or not args[1].isdigit():
-        error_msg = "Please send a valid number of days after the command, example: Key 2" if user_lang == 'en' else "Por favor envía un número válido de días después del comando, ejemplo: Key 2"
-        await message.reply_text(error_msg, reply_to_message_id=message.id)
-        return
-
-    dias = int(args[1])
-
-    key_random = ''.join(random.choices(string.ascii_letters + string.digits + "€#+*", k=8))
-    key_generada = f"AstroKey_#{key_random}"
-
-    username = message.from_user.username or "unknown"
-    fecha_expiracion = (datetime.now() + timedelta(days=dias)).strftime("%Y-%m-%d %H:%M:%S")
 
     user = MondB(idchat=user_id).queryUser()
     if user:
@@ -34,6 +22,18 @@ async def key_handler(client, message):
         lang = user_lang
 
     text_dict = text_en if lang == "en" else text_es
+
+    if len(args) < 2 or not args[1].isdigit():
+        await message.reply_text(text_dict['Key_usage'], reply_to_message_id=message.id)
+        return
+
+    dias = int(args[1])
+
+    key_random = ''.join(random.choices(string.ascii_letters + string.digits + "€#+*", k=8))
+    key_generada = f"AstroKey_#{key_random}"
+
+    username = message.from_user.username or "unknown"
+    fecha_expiracion = (datetime.now() + timedelta(days=dias)).strftime("%Y-%m-%d %H:%M:%S")
 
     respuesta = text_dict['key_system'].format(
         Key=key_generada,
