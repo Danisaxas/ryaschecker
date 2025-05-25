d7b5f3e0bbea0641771ecd3e63948286aa9812ac
Danisaxas <danluismay@gmail.com>
2025-05-25 11:47:07 -0400

    time

--- _MainChk.py
+++ _MainChk.py
@@ -1,24 +1,21 @@
 import asyncio
-from pyrogram.client import Client
+from pyrogram import Client
 from os import system
 from _date import (tokn, hasd, loogs)
 from complemet.func import expiracion_worker
 
 class _Astro:
 
-    def __init__(self, apiid: int, apihasd: str, token: str):
+    def __init__(self, apiid: int = None, apihasd: str = None, token: str = None):
         self.apiid = apiid
-        self.apihasd = apihasd
+        self.aapihasd = apihasd
         self.token = token
 
     def inictSecc(self):
-        if self.apiid is None or self.apihasd is None or self.token is None:
-            raise ValueError("apiid, apihasd y token no pueden ser None")
-
         self.client = Client(
             '_Astro',
             api_id=self.apiid,
-            api_hash=self.apihasd,
+            api_hash=self.aapihasd,
             bot_token=self.token,
             plugins=dict(root="complemet")
         )
@@ -34,7 +31,7 @@ async def main():
     # Lanzar worker en segundo plano para actualizar expiracion cada 60 segundos
     asyncio.create_task(expiracion_worker(60))
 
-    bot.run()
+    await bot.run()
 
 if __name__ == '__main__':
     try:
