df033e5656632be2d98a6fbeb2295c748d02ca7f
Danisaxas <danluismay@gmail.com>
2025-05-24 22:24:12 -0400

    update

--- complemet/func.py
+++ complemet/func.py
@@ -1,3 +1,5 @@
+import time
+import schedule
 from datetime import datetime, timedelta, timezone
 from classBot.MongoDB import MondB, queryUser
 
@@ -11,52 +13,44 @@ def formato_tiempo(delta: timedelta) -> str:
     segundos = total_segundos % 60
     return f"{dias}d-{horas:02d}h-{minutos:02d}m-{segundos:02d}s"
 
-def actualizar_expiracion_si_necesario(user_id: int):
-    db = MondB()
-    user = queryUser(user_id)
-    if not user:
-        return None
-
+def actualizar_expiracion_usuario(user):
+    user_id = user.get('_id')
     dias = user.get("dias", 0)
     since = user.get("since")
-    expiracion = user.get("expiracion")
     now = datetime.now(timezone.utc)
-
-    if not since:
-        return None
-
-    if isinstance(since, str):
-        since_dt = datetime.fromisoformat(since.replace("Z", "+00:00"))
-    else:
-        since_dt = since
-
-    tiempo_total = timedelta(days=dias)
-    tiempo_pasado = now - since_dt
-    tiempo_restante = tiempo_total - tiempo_pasado
-
-    if dias <= 0 or tiempo_restante.total_seconds() <= 0:
-        db._client['bot']['user'].update_one(
+    if not since or dias <= 0:
+        MondB()._client['bot']['user'].update_one(
             {"_id": user_id},
             {"$set": {"dias": 0, "expiracion": "0d-00h-00m-00s"}}
         )
         return
-
-    expiracion_str = formato_tiempo(tiempo_restante)
-
-    if expiracion != expiracion_str:
-        db._client['bot']['user'].update_one(
+    if isinstance(since, str):
+        since_dt = datetime.fromisoformat(since.replace("Z", "+00:00"))
+    else:
+        since_dt = since
+    tiempo_total = timedelta(days=dias)
+    tiempo_pasado = now - since_dt
+    tiempo_restante = tiempo_total - tiempo_pasado
+    if tiempo_restante.total_seconds() <= 0:
+        MondB()._client['bot']['user'].update_one(
             {"_id": user_id},
-            {"$set": {"expiracion": expiracion_str}}
+            {"$set": {"dias": 0, "expiracion": "0d-00h-00m-00s"}}
         )
+        return
+    expiracion_str = formato_tiempo(tiempo_restante)
+    MondB()._client['bot']['user'].update_one(
+        {"_id": user_id},
+        {"$set": {"expiracion": expiracion_str}}
+    )
 
-def tiempo_restante(user_id: int) -> str:
-    actualizar_expiracion_si_necesario(user_id)
-    user = queryUser(user_id)
-    if not user:
-        return "0d-00h-00m-00s"
+def actualizar_todas_expiraciones():
+    db = MondB()
+    usuarios = db._db['user'].find({"dias": {"$gt": 0}})
+    for user in usuarios:
+        actualizar_expiracion_usuario(user)
 
-    expiracion = user.get("expiracion")
-    if not expiracion:
-        return "0d-00h-00m-00s"
-
-    return expiracion
+def iniciar_scheduler(time_interval_seconds=1):
+    schedule.every(time_interval_seconds).seconds.do(actualizar_todas_expiraciones)
+    while True:
+        schedule.run_pending()
+        time.sleep(0.5)
