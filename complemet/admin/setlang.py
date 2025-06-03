cb5190683217caf4dde67ccef932b431e421f2c0
Danisaxas <danluismay@gmail.com>
2025-06-02 20:59:13 -0400

    update

--- complemet/admin/setlang.py
+++ complemet/admin/setlang.py
@@ -2,7 +2,13 @@ from _date import *
 from classBot.MongoDB import MondB
 from Source_pack.TextAll import en as text_en
 from Source_pack.TextAll import es as text_es
+from Source_pack.TextAll import pt as text_pt
+from Source_pack.TextAll import ru as text_ru
+from Source_pack.TextAll import zh as text_zh
+from Source_pack.TextAll import ko as text_ko
 from pyrogram.types import Message
+
+IDIOMAS_VALIDOS = ["es", "en", "pt", "ru", "zh", "ko"]
 
 @Astro("setlang")
 async def comando_setlang(client, message: Message):
@@ -11,8 +17,21 @@ async def comando_setlang(client, message: Message):
 
     lang = user_data.get("lang", "es") if user_data else "es"
     lang = lang.lower()
-    lang = 'en' if lang.startswith('en') else 'es'
-    text = text_en if lang == "en" else text_es
+    lang = 'en' if lang.startswith('en') else lang
+    lang = 'pt' if lang.startswith('pt') else lang
+    lang = 'ru' if lang.startswith('ru') else lang
+    lang = 'zh' if lang.startswith('zh') else lang
+    lang = 'ko' if lang.startswith('ko') else lang
+    lang = 'es' if lang not in IDIOMAS_VALIDOS else lang
+
+    text = {
+        "es": text_es,
+        "en": text_en,
+        "pt": text_pt,
+        "ru": text_ru,
+        "zh": text_zh,
+        "ko": text_ko
+    }[lang]
 
     if str(user_id) != str(owner):
         await message.reply_text(
@@ -33,7 +52,7 @@ async def comando_setlang(client, message: Message):
         return
 
     nuevo_idioma = args[2].lower()
-    if nuevo_idioma not in ["es", "en"]:
+    if nuevo_idioma not in IDIOMAS_VALIDOS:
         await message.reply_text(text['setlang_invalid_lang'], reply_to_message_id=message.id)
         return
 
