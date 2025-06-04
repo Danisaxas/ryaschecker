0d50e5ad366384f3b95d72cb0dc162bfaac6e26b
Danisaxas <danluismay@gmail.com>
2025-06-04 15:49:11 -0400

    as

--- complemet/Inicio/start.py
+++ complemet/Inicio/start.py
@@ -27,12 +27,14 @@ async def start(client, message):
 
     message_text = start_text.format(caracas_time=caracas_time, username=username, idioma_actual=idioma_actual)
 
-    # Usamos el ID del mensaje actual como respuesta (es decir, si el comando /start se ejecuta como respuesta a otro mensaje)
-    reply_to_message_id = message.message_id  # Usamos el ID del mensaje actual
+    # Verificar si el mensaje tiene un ID (esto es importante para saber si se puede hacer una respuesta)
+    reply_to_message_id = None
+    if hasattr(message, "message_id"):
+        reply_to_message_id = message.message_id  # Usamos el ID del mensaje actual
 
     await client.send_message(
         chat_id=user_id,
         text=message_text,
         reply_markup=InlineKeyboardMarkup(mainstart_buttons),
-        reply_to_message_id=reply_to_message_id  # Responde al mensaje que ejecutó /start
+        reply_to_message_id=reply_to_message_id  # Responde al mensaje actual si tiene un ID
     )
