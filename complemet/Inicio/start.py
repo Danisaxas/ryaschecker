6207f842f48e1ddc5658136d3f0997335df1feb8
Danisaxas <danluismay@gmail.com>
2025-06-04 12:15:32 -0400

    Actualizar start.py

--- complemet/Inicio/start.py
+++ complemet/Inicio/start.py
@@ -1,44 +1,18 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import json
-import os
+from _date import *
 from classBot.MongoDB import MondB
 from datetime import datetime
 import pytz
-from _date import *
 
-@Astro("Start")
+@Astro("start")
 async def start(client, message):
     user_id = message.chat.id
     username = message.from_user.username or "Usuario"
 
-    user = MondB(idchat=user_id).queryUser()
-    lang = (user.get("lang") if user else "es") or "es"
-    lang = lang.lower()
-
-    locales_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "locales")
-    buttons_path = os.path.join(locales_path, "button_layouts")
-
-    lang_file = os.path.join(locales_path, f"{lang}.json")
-    buttons_file = os.path.join(buttons_path, f"{lang}.json")
-
-    # Verificar si el archivo de idioma existe
-    if os.path.exists(lang_file):
-        with open(lang_file, "r", encoding="utf-8") as f:
-            data = json.load(f)
-    else:
-        with open(os.path.join(locales_path, "es.json"), "r", encoding="utf-8") as f:
-            data = json.load(f)
-
-    # Verificar si el archivo de botones existe
-    if os.path.exists(buttons_file):
-        with open(buttons_file, "r", encoding="utf-8") as f:
-            buttons_data = json.load(f)
-    else:
-        with open(os.path.join(buttons_path, "es.json"), "r", encoding="utf-8") as f:
-            buttons_data = json.load(f)
+    data, buttons_data = load_language_file(user_id)
 
     start_text = data.get("startx", "¡Bienvenido!")
-    
+
     mainstart_buttons = [
         [InlineKeyboardButton(button['text'], callback_data=button['callback_data']) for button in row]
         for row in buttons_data.get("mainstart", [])
@@ -47,14 +21,12 @@ async def start(client, message):
     timezone = pytz.timezone("America/Caracas")
     caracas_time = datetime.now(timezone).strftime("%H:%M:%S")
 
+    lang = (MondB(idchat=user_id).queryUser().get("lang") if user else "es") or "es"
     idioma_actual = f"{LANGUAGES_FLAGS.get(lang, '🏳️‍🌈')} {lang.upper()}"
 
     message_text = start_text.format(caracas_time=caracas_time, username=username, idioma_actual=idioma_actual)
 
-    if message.reply_to_message:
-        reply_to_message_id = message.reply_to_message.message_id
-    else:
-        reply_to_message_id = None
+    reply_to_message_id = message.reply_to_message.message_id if message.reply_to_message else None
 
     await client.send_message(
         chat_id=user_id,
