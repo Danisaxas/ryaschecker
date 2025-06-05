9fc70c7d7c1bca6d7f2e93d43800f73e1ec7a67a
Danisaxas <danluismay@gmail.com>
2025-06-05 17:42:44 -0400

    xas

--- complemet/handlers/tools.py
+++ complemet/handlers/tools.py
@@ -41,3 +41,23 @@ async def handle_next(client, callback_query):
 
     await callback_query.message.edit_text(message, reply_markup=InlineKeyboardMarkup(tools_back_buttons))
     await callback_query.answer()
+
+@AstroButton("^tools_1$")
+async def handle_tools_1(client, callback_query):
+    user = callback_query.from_user
+    user_id = user.id
+
+    lang_data, buttons_data = load_language_file(user_id)
+
+    tools_text = lang_data.get("tools", "")
+    message = tools_text.format(
+        username=user.username or user.first_name
+    )
+
+    atras_buttons = [
+        [InlineKeyboardButton(button['text'], callback_data=button['callback_data']) for button in row]
+        for row in buttons_data.get("atras", [])
+    ]
+
+    await callback_query.message.edit_text(message, reply_markup=InlineKeyboardMarkup(atras_buttons))
+    await callback_query.answer()
