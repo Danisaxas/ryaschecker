7bfbfeaf2f33a84ebd77721817897118320f82ae
Danisaxas <danluismay@gmail.com>
2025-06-05 12:10:08 -0400

    xas

---
+++ complemet/handlers/tools.py
@@ -0,0 +1,23 @@
+from _date import *
+from classBot.MongoDB import MondB
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+@AstroButton("^tools$")
+async def handle_tools(client, callback_query):
+    user = callback_query.from_user
+    user_id = user.id
+
+    lang_data, buttons_data = load_language_file(user_id)
+
+    tools_text = lang_data.get("tools", "")
+    message = tools_text.format(
+        username=user.username or user.first_name
+    )
+
+    atras_buttons = [
+        [InlineKeyboardButton(button['text'], callback_data=button['callback_data']) for button in row]
+        for row in buttons_data.get("atras", [])
+    ]
+
+    await callback_query.message.edit_text(message, reply_markup=InlineKeyboardMarkup(atras_buttons))
+    await callback_query.answer()
